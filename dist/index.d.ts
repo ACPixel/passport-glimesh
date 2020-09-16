b08be42273e25edfd59a900eb24dafd597c6f587
ACPixel <acpixel@acpixel.com>
2020-09-16 14:43:29 -0700

    declarations

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Module dependencies.
+ */
+import OAuth2Strategy from "./oauth2";
+/**
+ * Expose constructors.
+ */
+export { OAuth2Strategy as Strategy };
---
+++ dist/oauth2.d.ts
@@ -0,0 +1,11 @@
+/**
+ * `Strategy` constructor.
+ * @param {Object} options
+ * @param {Function} verify
+ * @api public
+ */
+declare function Strategy(options: any, verify: any): void;
+/**
+ * Expose `Strategy`.
+ */
+export default Strategy;
